ebda668e8db864763cd8cd165729dc531c0e02e4
Philip Kosilov <philipkosilov@gmail.com>
2022-09-01 18:45:53 +0300

    Add settings feature

--- feature_favorites/src/main/java/com/example/bookgo/feature_favorites/presentation/FavoritesFragment.kt
+++ feature_favorites/src/main/java/com/example/bookgo/feature_favorites/presentation/FavoritesFragment.kt
@@ -7,9 +7,6 @@ import androidx.fragment.app.Fragment
 import com.example.bookgo.feature_favorites.R
 
 class FavoritesFragment: Fragment(R.layout.fragment_favorites) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.i("logcat", "onCreate: wow in fav")
-    }
+
 
 }
---
+++ feature_settings/src/main/java/com/example/bookgo/feature_settings/presentation/SettingsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.bookgo.feature_settings.presentation
+
+import androidx.fragment.app.Fragment
+import com.example.bookgo.feature_settings.R
+
+class SettingsFragment: Fragment(R.layout.fragment_settings) {
+}
